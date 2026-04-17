e56badf34419925fd5a50f8be6e44ef91a92e701
ibtsamali1 <ibtsam0408@gmail.com>
2026-04-18 01:05:22 +0500

    a

--- src/demo_saas/views.py
+++ src/demo_saas/views.py
@@ -8,19 +8,17 @@ def home_page_view(request ):
 def base_page(request):
     return render(request, 'base.html')
 
-def home_page(request):
-    query_Set=page_visits.objects.all()
-    html_template='home.html'
-    path_total=page_visits.objects.filter(path=request.path)
-    my_context={
-        "quary_set":query_Set,
-        # "page_vists_count":query_Set.count(),
-        "path_counter": path_total.count(),   
-    }
+# def home_page(request):
+#     query_Set=page_visits.objects.all()
+#     html_template='home.html'
+#     path_total=page_visits.objects.filter(path=request.path)
+#     my_context={
+#         "quary_set":query_Set,
+#         # "page_vists_count":query_Set.count(),
+#         "path_counter": path_total.count(),   
+#     }
     
-    # path= request.path
-    page_visits.objects.create(path=request.path)
-    return render(request, html_template,my_context)
+    # # path= request.path
+    # page_visits.objects.create(path=request.path)
+    # return render(request, html_template,my_context)
 
-def about_page(request,*args, **kwargs):
-    return home_page (request , *args, **kwargs)
