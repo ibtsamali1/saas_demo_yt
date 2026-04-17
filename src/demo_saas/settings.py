70bd4d0e9f2b43436ac0b930babda7689a4df2a5
ibtsamali1 <ibtsam0408@gmail.com>
2026-04-18 02:28:43 +0500

    SECRET KEY

--- src/demo_saas/settings.py
+++ src/demo_saas/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fg)#tp4kt77h(lci=3i$ssd2^vr(3sh5lau=x#1#(svfzf!9v&"
+SECRET_KEY = config("DJANGO_SCRECT_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() =='true'
-
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() =='true'
+DEBUG=config("DJANGO_DEBUG",cast=bool)
 
 print("Debug",DEBUG, type(DEBUG))
 
